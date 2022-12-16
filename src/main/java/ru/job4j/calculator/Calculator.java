77d07616ba3f8e78c0e034b79b6ea6d13d2687c7
Valerii Komar <inrarad@gmail.com>
2022-12-16 19:24:09 +0000

    4.1. Аргументы [#295521]

---
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -0,0 +1,14 @@
+package ru.job4j.calculator;
+
+public class Calculator {
+    public static void main(String[] args) {
+        int one = 1;
+        int two = 2;
+        int result = one + two;
+        System.out.println(result);
+        one = 10;
+        two = 11;
+        result = one + two;
+        System.out.println(result);
+    }
+}
