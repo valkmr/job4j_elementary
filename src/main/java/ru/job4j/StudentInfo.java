04b038f72f0fb832253be13565e64c3ed2e531d7
Valerii Komar <inrarad@gmail.com>
2022-12-18 21:59:06 +0000

    Модульные тесты. [#159435]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Valerii Komar");
+        System.out.println("19.11.1988");
+    }
+}
