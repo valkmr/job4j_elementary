f972d3ffcf95b8d9332013162b6c7b4e5628b317
Valerii Komar <inrarad@gmail.com>
2022-12-24 16:40:54 +0000

    Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -13,5 +13,14 @@ public class ArrayDefinition {
         float[] prices = new float[40];
         System.out.println("prices length " + prices.length);
 
+        String[] names = new String[4];
+        names[0] = "Valerii";
+        names[1] = "Anna";
+        names[2] = "Veles";
+        names[3] = "Vesta";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
