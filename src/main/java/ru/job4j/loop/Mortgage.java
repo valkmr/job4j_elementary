704d6f535848103ad191a041cfca16cbb89836f1
Valerii Komar <inrarad@gmail.com>
2022-12-24 12:04:02 +0000

    Ипотека [#156315 #374204]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -3,13 +3,10 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(double amount, int salary, double percent) {
         int year = 0;
-        if (salary > amount * percent / 100) {
-            year++;
-            while (amount + amount * percent / 100 > salary) {
+            while (amount > 0) {
                 amount += amount * percent / 100 - salary;
                 year++;
             }
-        }
         return year;
     }
 
