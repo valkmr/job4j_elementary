416c66d21a67fa71d3ba6c34e27d2e1a33caefdd
Valerii Komar <inrarad@gmail.com>
2022-12-25 20:37:45 +0000

    Размер массива [#333582]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -6,13 +6,10 @@ public class ArrayDefinition {
     public static void main(String[] args) {
         short[] ages = new short[10];
         System.out.println("ages length " + ages.length);
-
         String[] surnames = new String[100500];
         System.out.println("surnames length " + surnames.length);
-
         float[] prices = new float[40];
         System.out.println("prices length " + prices.length);
-
         String[] names = new String[4];
         names[0] = "Valerii";
         names[1] = "Anna";
