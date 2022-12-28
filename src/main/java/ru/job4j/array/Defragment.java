34cb36dca68b709a6d6ffb23ed08c1cfecfa5258
Valerii Komar <inrarad@gmail.com>
2022-12-28 13:16:00 +0000

    Дефрагментация массива [#225]

---
+++ src/main/java/ru/job4j/array/Defragment.java
@@ -0,0 +1,18 @@
+package ru.job4j.array;
+
+public class Defragment {
+    public static String[] compress(String[] array) {
+        for (int index = 0; index < array.length; index++) {
+            if (array[index] == null) {
+                for (int i = index + 1; i < array.length; i++) {
+                    if (array[i] != null) {
+                        array[index] = array[i];
+                        array[i] = null;
+                        break;
+                    }
+                }
+            }
+        }
+        return array;
+    }
+}
