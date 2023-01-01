a500523a55b3cf91ff927d12371c534e52c91f14
Valerii Komar <inrarad@gmail.com>
2023-01-01 12:23:06 +0000

    Перегрузить метод max для трех чисел [#122644]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -4,4 +4,14 @@ public class Max {
     public static int max(int left, int right) {
         return left <= right ? right : left;
     }
+
+    public static int max(int first, int second, int third) {
+        int maxtwo = max(first, second);
+        return maxtwo >= third ? maxtwo : third;
+    }
+
+    public static int max(int first, int second, int third, int fourth) {
+        int maxthree = max(first, second, third);
+        return maxthree >= fourth ? maxthree : third;
+    }
 }
--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MaxTest {
-
     @Test
     public void whenL1R2Then2() {
         int left = -3;
@@ -32,4 +31,26 @@ public class MaxTest {
         int expected = 0;
         assertThat(result).isEqualTo(expected);
     }
+
+    @Test
+    public void whenF5S4T9Then9() {
+        int first = 5;
+        int second = 4;
+        int third = 9;
+        int result = Max.max(first, second, third);
+        int expected = 9;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenF0S4T0F4Then4() {
+        int first = 0;
+        int second = 4;
+        int third = 0;
+        int fourth = 4;
+        int result = Max.max(first, second, third, fourth);
+        int expected = 4;
+        assertThat(result).isEqualTo(expected);
+    }
+
 }
